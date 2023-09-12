e3a36ad04face02b25a0255c46194424b269c499
Gunarshild <144447063+Gunarshild@users.noreply.github.com>
2023-09-12 11:15:14 +0300

    Add files via upload

---
+++ tests_elina_gunars.py
@@ -0,0 +1,20 @@
+#30 dienas Python programmēšanas valodā
+name = 'Elina'
+second_name = 'Teilane'
+full_name = name + second_name
+valsts = 'Latvija'
+pilseta = 'Riga'
+Vecums = 100
+gads = 2020
+a = e = f = 2
+print(name,'\n',second_name,'\n',full_name,'\n',valsts,'\n',pilseta,'\n',Vecums,'\n',gads,'\n',a,'\n',e,'\n',f)
+print(name,type(name))
+print(second_name,type(second_name))
+print(full_name,type(full_name))
+print(valsts,type(valsts))
+print(pilseta,type(pilseta))
+print(Vecums,type(Vecums))
+print(gads,type(gads))
+print(a,type(a))
+print(e,type(e))
+print(f,type(f))
