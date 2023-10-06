bfb45153989098dadbfc3beb8208c7a988466ada
Gunarshild <144447063+Gunarshild@users.noreply.github.com>
2023-10-06 12:08:26 +0300

    Add files via upload

---
+++ ievarijums.py
@@ -0,0 +1,30 @@
+
+cukurs = 1.3, ('eiro/1kg')
+udens = 
+citrons = 0.86, ('eiro/kg')
+mandeļa_ekstrakts = 9.67, ('eiro/118ml')
+kanelis = 0.63 ('eiro/25g')
+
+
+
+
+
+
+
+
+
+recepte_aboli = float(input('cik kg ābolu vajag receptei? (kg) '))
+recepte_cukurs = float(input('cik kg cukura vajag receptei? (kg) '))
+recepte_udens = float(input('cik ml ūdens vajag receptei? (ml) '))
+recepte_citrons = float(input('cik citronus vajag receptei? (gab) '))
+recepte_mandelu_ekstrakts = float(input('cik ml mandeļu ekstraktu vajag receptei? (ml) '))
+recepte_kanelis = float(input('cik g kanēļa vajag receptei? (g) '))
+
+
+
+
+
+
+
+
+
